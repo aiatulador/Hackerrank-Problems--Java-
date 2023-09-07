44a88635e50986887b6260426d1b8a6156e18d6b
Aiatul Al-amin Ador <aiatulador75@gmail.com>
2023-09-08 04:29:58 +0600

    -

---
+++ JavaOutputFormatting.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class JavaOutputFormatting {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("================================");
+    for (int i = 0; i < 3; i++) {
+      String s1 = sc.next();
+      System.out.print(s1);
+      int x = sc.nextInt();
+      for (int j = s1.length(); j < 15; j++) {
+        System.out.print(' ');
+      }
+      Integer integer = new Integer(x);
+      String sInteger = integer.toString();
+      for (int j = sInteger.length(); j < 3; j++) {
+        System.out.print('0');
+      }
+      System.out.println(sInteger);
+    }
+    System.out.println("================================");
+
+  }
+}
